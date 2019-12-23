cd0e262ab5b9ebae9bce48201122c347955f9b11
Nicolas Fernandez <nicolas.fernandez@outlook.cl>
2019-12-23 15:27:25 -0300

    first commit

---
+++ src/main/java/com/example/springcoursesmongo/SpringCoursesMongoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcoursesmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCoursesMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCoursesMongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springcoursesmongo/SpringCoursesMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcoursesmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCoursesMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
